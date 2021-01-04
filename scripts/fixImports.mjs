cb2ef5f9c8ed3c2e14a95bd541253b293554941a
Пронин Егор <mrzillagold@gmail.com>
2021-01-04 20:32:53 +1000

    Refactoring / Using Babel for builds

---
+++ scripts/fixImports.mjs
@@ -0,0 +1,22 @@
+import { promises as fs } from "fs";
+
+const BUILD_DIR = "./dist";
+
+fs.readdir(BUILD_DIR)
+    .then((files) => {
+        files.forEach(async (file) => {
+            if (file.endsWith(".js")) {
+                const filePath = `${BUILD_DIR}/${file}`;
+
+                fs.readFile(filePath)
+                    .then((file) => {
+                        file = file.toString();
+
+                        file = file.replace(/\.default/gm, "")
+                            .replace(/\.mjs/gm, ".js");
+
+                        fs.writeFile(filePath, file);
+                    });
+            }
+        })
+    });
